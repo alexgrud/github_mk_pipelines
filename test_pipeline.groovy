58d641c1afc4426115da40a266e140e7e1904319
alexgrud <30340099+alexgrud@users.noreply.github.com>
2018-04-12 11:57:21 +0300

    Update test_pipeline.groovy

--- test_pipeline.groovy
+++ test_pipeline.groovy
@@ -1,4 +1,11 @@
 common = new com.mirantis.mk.Common()
 node{
+	def venv
+	def venvPepper
+	def outputs = [:]
+	def workspace = common.getWorkspace()
+        venv = "${workspace}/venv"
+        venvPepper = "${workspace}/venvPepper"
 	println("Test message")
+	println(venvPepper)
 }
