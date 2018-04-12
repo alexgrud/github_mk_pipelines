6ae7a5ea47a528bb380d7dbe90eab13f34f89779
alexgrud <30340099+alexgrud@users.noreply.github.com>
2018-04-12 12:17:15 +0300

    Update test_pipeline.groovy

--- test_pipeline.groovy
+++ test_pipeline.groovy
@@ -8,4 +8,8 @@ node{
         venvPepper = "${workspace}/venvPepper"
 	println("Test message")
 	println(venvPepper)
+	saltMasterHost="172.17.49.168"
+	SALT_MASTER_URL = "http://${saltMasterHost}:6969"
+	println(SALT_MASTER_URL)
+
 }
